4e2b34673de3067722acc2fec0a27f8b5bf9eabc
Damiano Salvi <salvi@intre.it>
2022-03-10 15:28:06 +0100

    ✨ Solve Tic Tac Toe kata

---
+++ kotlin/src/sal/SalTrisTest.kt
@@ -0,0 +1,51 @@
+package sal
+
+import org.junit.jupiter.api.Test
+import sal.Location.*
+
+
+class SalTrisTest {
+    @Test
+    fun `play until tied`() {
+        with(SalTris()) {
+            play(NORTHWEST)
+            play(NORTH)
+            play(NORTHEAST)
+            play(WEST)
+            play(SOUTHWEST)
+            play(CENTER)
+            play(EAST)
+            play(SOUTHEAST)
+            play(SOUTH)
+            println(this)
+            assert(isGameOver())
+        }
+    }
+
+    @Test
+    fun `play until X wins on first row`() {
+        with(SalTris()) {
+            play(NORTHWEST)
+            play(SOUTHWEST)
+            play(NORTH)
+            play(SOUTH)
+            play(NORTHEAST)
+            println(this)
+            assert(isGameOver())
+        }
+    }
+
+    @Test
+    fun `play until O wins on diagonal`() {
+        with(SalTris()) {
+            play(NORTHWEST)
+            play(CENTER)
+            play(SOUTHEAST)
+            play(NORTHEAST)
+            play(NORTH)
+            play(SOUTHWEST)
+            println(this)
+            assert(isGameOver())
+        }
+    }
+}
