b554e8470922cea405e9c743dfd630022b7deaa3
fra <matteo.franchini@intre.it>
2022-03-10 15:33:20 +0100

    frankata

---
+++ TypeScript/fra/TicTacToe.ts
@@ -0,0 +1,31 @@
+export type Player = 'O' | 'X'
+
+const grid = [['','',''],['','',''],['','','']]
+
+const getCell = (position: {y:number, x:number}): string => {
+    return grid[position.y][position.x]
+}
+
+const makeAMove = (player: Player, position: {y:number, x:number})=>{
+    if(getCell(position) !== player){
+        grid[position.y][position.x] = player
+    }
+}
+
+const getWinner = () : Player | undefined =>{
+    return grid.filter((row)=>row.every(cell => cell && cell === row[0]))[0][0] as Player
+}
+
+const pickPlayer = (player?: Player) : Player=>{
+    if(!player) return Math.random()>0.5? 'O': 'X'
+    if(player === 'O') return 'X'
+    else return 'O'
+}
+const moves =  [['X','X','O'],['O','X','O'],['X','X','X']]
+const game = (player?:Player) : Player =>{
+    const winner = getWinner()
+    if(winner) return winner
+
+    makeAMove(player | pickPlayer(), )
+
+}
