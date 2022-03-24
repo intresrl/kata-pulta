43a594793c1478ee38faa9b1572776995cc64a97
Marcato Luca <marcato@intre.it>
2022-03-24 16:10:23 +0100

    nothing works

--- java/wardrobe-kata/mar/Wardrobe.java
+++ java/wardrobe-kata/mar/Wardrobe.java
@@ -1,6 +1,39 @@
-package mar;
+import kotlin.collections.EmptyList;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Wardrobe {
+    final int WALL_LENGTH = 250;
+
+    boolean matchWall(List<Integer> pieces) {
+        final int wardrobeLength = pieces.stream().mapToInt(Integer::intValue).sum();
+        return wardrobeLength == WALL_LENGTH;
+    }
+
+    List<List<Integer>> permutatePieces(List<Integer> pieces) {
+        List<List<Integer>> perms = new ArrayList<>();
+        for(int i=0; i < pieces.size(); i++) {
+            perms.add(perm(pieces, new ArrayList<>()));
+        }
+        return perms;
+    }
+
+    private List<Integer> perm(List<Integer> pieces, List<Integer> oldPerm) {
+        if(pieces.size() == 0) {
+            return oldPerm;
+        }
+        oldPerm.add(pieces.get(0));
+        final List<Integer> newPieces = pieces.subList(1, pieces.size());
+
+        return perm(newPieces, oldPerm);
+    }
+
+    public List<List<Integer>> getAllValidPermutations(List<Integer> pieces) {
+        return permutatePieces(pieces).stream().filter(this::matchWall).collect(Collectors.toList());
+    }
+
 }
 
 
--- java/wardrobe-kata/mar/WardrobeTest.java
+++ java/wardrobe-kata/mar/WardrobeTest.java
@@ -1,15 +1,56 @@
-package mar;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class WardrobeTest {
+    /**
+        The wardrobe elements sizes: 50cm, 75cm, 100cm, and 120cm.
+        The wall total length of 250cm.
+
+        Write a function that returns all combinations of wardrobe elements
+        that exactly fill the wall.
+     **/
 
     @Test
-    public void testFail() {
-        fail();
+    public void combinationMatchSpace() {
+        final Wardrobe wardrobe = new Wardrobe();
+
+        assertEquals(true, wardrobe.matchWall(Arrays.asList(100, 50, 100)));
+    }
+
+    @Test
+    public void combinationExceedSpace() {
+        final Wardrobe wardrobe = new Wardrobe();
+
+        assertEquals(false, wardrobe.matchWall(Arrays.asList(120, 50, 100)));
+    }
+
+    @Test
+    public void permutate1Value() {
+        List<List<Integer>> expectedPermutations = List.of(List.of(120));
+        final Wardrobe wardrobe = new Wardrobe();
+
+        List<List<Integer>> permutations = wardrobe.permutatePieces(List.of(120));
+
+        assertEquals(expectedPermutations, permutations);
+    }
+
+    @Test
+    public void permutate2Values() {
+        List<List<Integer>> expectedPermutations = Arrays.asList(
+                Arrays.asList(120,120),
+                Arrays.asList(120,50),
+                Arrays.asList(50,50)
+        );
+        final Wardrobe wardrobe = new Wardrobe();
+
+        List<List<Integer>> permutations = wardrobe.permutatePieces(Arrays.asList(120, 50));
+
+        assertEquals(expectedPermutations, permutations);
     }
 }
