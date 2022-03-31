b4de76556c24d939d8b6faa86981e62d508a253b
Lorenzo <testa.lorenzo@hotmail.com>
2022-03-31 15:16:00 +0200

    Cell, World and main

--- java/game-of-life/GameOfLife.java
+++ java/game-of-life/GameOfLife.java
@@ -1,7 +1,15 @@
+import java.util.ArrayList;
+
 public class GameOfLife {
 
     public static void main(String []args){
         System.out.println("ciao");
+
+        String initial = "..x," + "...," + "...";
+
+        new World(initial);
+
+
     }
 
 
---
+++ java/game-of-life/World.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class World {
+
+    List<List<Cell>> status;
+
+    public World(String initial) {
+        int length = initial.length();
+        double dimension = Math.sqrt(length);
+
+        for(String singleCell : List.of(initial)){
+
+        }
+    }
+
+}
