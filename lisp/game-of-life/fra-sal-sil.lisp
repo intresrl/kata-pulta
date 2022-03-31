8a4482915889e7dd1d284175f8dcb7ae1b5d67c2
Damiano Salvi <salvi@intre.it>
2022-03-31 15:15:44 +0200

    🚧 Game of Life WIP

--- lisp/game-of-life/fra-sal-sil.lisp
+++ lisp/game-of-life/fra-sal-sil.lisp
@@ -4,18 +4,36 @@
 ; Each cell with one or no neighbors dies, as if by  solitude.
 ; Each cell with four or more neighbors dies, as if by overpopulation.
 ; Each cell with two or three neighbors survives.
-;
+; 
 ; For a space that is empty or unpopulated:
 ; Each cell with three neighbors becomes populated.
 
+(defun evolveCell (cell aliveNeighbors) 
+    (cond 
+      ((eq cell 0) 
+        (cond 
+          ((eq aliveNeighbors 3) 1)
+          (t 0)
+        )
+      )
+      ((eq cell 1) 
+        (cond 
+          ((eq aliveNeighbors 2) 1)
+          ((eq aliveNeighbors 3) 1)
+          (t 0)
+        )
+      )
+    )
+)
+(let (
+   (grid '(
+     (0 1 0)
+     (1 1 0)
+     (1 0 1)
+   ))
+   (y '(a b c))
+ )
+ (print (car grid))
+ (print (cdr grid))
 
-
-(defun bottles (x)
-  (loop for bottles from x downto 1
-        do (format t "~a bottle~:p of beer on the wall~@
-                      ~:*~a bottle~:p of beer~@
-                      Take one down, pass it around~@
-                      ~V[No more~:;~:*~a bottle~:p of~] beer on the wall~2%"
-                   bottles (1- bottles))))
-
-(bottles 99)
+)
