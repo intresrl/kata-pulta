f39668daabfb51db136f526a40b346e7da0f9901
Marcato Luca <marcato@intre.it>
2022-03-31 15:43:48 +0200

    init world & toString

--- java/game-of-life/Cell.java
+++ java/game-of-life/Cell.java
@@ -1,3 +1,12 @@
 public class Cell {
+    private boolean isAlive;
 
+    public Cell(boolean isAlive) {
+        this.isAlive = isAlive;
+    }
+
+    @Override
+    public String toString() {
+        return isAlive ? "x" : ".";
+    }
 }
--- java/game-of-life/World.java
+++ java/game-of-life/World.java
@@ -2,18 +2,29 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class World {
 
-    List<List<Cell>> status;
+    List<List<Cell>> status = new ArrayList<>();
 
     public World(String initial) {
-        int length = initial.length();
-        double dimension = Math.sqrt(length);
-
-        for(String singleCell : List.of(initial)){
-
+        for(String stringRow : initial.split(",")){
+            final ArrayList<Cell> row = new ArrayList<>();
+            for(char cell : stringRow.toCharArray()) {
+                row.add(new Cell(cell != '.'));
+            }
+            status.add(row);
         }
     }
 
+    @Override
+    public String toString() {
+        return status.stream()
+                .map(x -> x.stream()
+                        .map(Cell::toString)
+                        .collect(Collectors.joining())
+                )
+                .collect(Collectors.joining(","));
+    }
 }
