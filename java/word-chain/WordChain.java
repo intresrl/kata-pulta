f440e731b0f26417a9a679cd834799435203a81b
Matteo PAL <matteo.palmieri@intre.it>
2022-04-07 17:19:18 +0200

    Words Chain - words iteration

--- java/word-chain/WordChain.java
+++ java/word-chain/WordChain.java
@@ -1,22 +1,51 @@
+
+
+
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
 
 public class WordChain {
-    String firstWord="cat";
-    String lastWord="dog";
+    static String firstWord="cat";
+    static String lastWord="dog";
+    
     public static void main(String []args) throws Exception{
+
+        String everything;
+
+        extracted(firstWord.length());
+
+
+    }
+
+    private static void extracted(int lenght, List<String> words) throws IOException {
+        String everything;
         try(BufferedReader br = new BufferedReader(new FileReader("dictionary.txt"))) {
             StringBuilder sb = new StringBuilder();
             String line = br.readLine();
 
-            while (line != null) {
-                sb.append(line);
-                sb.append(System.lineSeparator());
-                line = br.readLine();
+            for (String word : words) {
+                if (word.length() == lenght) {
+                    sb.append(line);
+                    sb.append(System.lineSeparator());
+                    line = br.readLine();
+                }
             }
-            String everything = sb.toString();
+            everything = sb.toString();
         }
+    }
 
-        System.out.println("ciao");
+    private static List<String> getGraph(List<String> words, String startingWord) {
+        words.forEach(word -> {
+            int mismatchCount = 0;
+
+            for (int i = 0; i < word.length(); i++) {
+                mismatchCount = word.charAt(i) != startingWord.charAt(i) ? mismatchCount++ : mismatchCount;
+                if (mismatchCount > 1) {
+
+                }
+            }
+        });
     }
 }
