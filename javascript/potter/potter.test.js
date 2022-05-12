5f2afc6553174d003a08f0244843eceb77af654d
Damiano Salvi <salvi@intre.it>
2022-05-12 16:07:26 +0200

    ✨ Harry Potter - 1st test

--- javascript/potter/potter.test.js
+++ javascript/potter/potter.test.js
@@ -1,5 +1,12 @@
-describe('i test',()=>{
-    it('says hello', ()=>{
-        expect("Hello").not.toEqual("World")
-    })
+import {resolve} from "./potter";
+
+
+const sapiens = (input, expectedOutput) => {
+    expect(resolve(input)).toEqual(expectedOutput)
+};
+
+describe('Harry Potter bookshop', () => {
+
+    it('one book', () => sapiens("A", 8))
+
 })
