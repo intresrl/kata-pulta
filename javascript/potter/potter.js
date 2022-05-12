050fc7ab0c920416256cdfac8b9a89f28a41c367
Gabriel_Silvas <gabrielgeorge.silvas97@gmail.com>
2022-05-12 17:09:41 +0200

    🚧 add test for discount given two different books

--- javascript/potter/potter.js
+++ javascript/potter/potter.js
@@ -1,79 +1,92 @@
 /**
-To try and encourage more sales of the 5 different Harry Potter books they sell,
+ To try and encourage more sales of the 5 different Harry Potter books they sell,
  a bookshop has decided to offer discounts of multiple-book purchases.
 
-For sake of simplicity, the books will be represented by the simple strings "A", "B", "C", "D", "E".
+ For sake of simplicity, the books will be represented by the simple strings "A", "B", "C", "D", "E".
 
-One copy of any of the five books costs 8 EUR.
+ One copy of any of the five books costs 8 EUR.
 
  ----------------
  To pass the first test, just focus on pricing a single book
  ----------------
 
-If you try to buy a book not stated in the previous list you get 'ERROR'
+ If you try to buy a book not stated in the previous list you get 'ERROR'
 
  ----------------
  To pass the second test, check if the book is different from [A,B,C,D,E]. If it is, return 'ERROR'
  ----------------
 
-If you buy two copies of a book each copy costs 8 EUR.
+ If you buy two copies of a book each copy costs 8 EUR.
 
  ----------------
  To pass the third test, consider that you may receive multiple books as input.
  You should split the input string on whitespaces.
  ----------------
 
-If, however, you buy two different books, you get a 5% discount on those two books.
+ If, however, you buy two different books, you get a 5% discount on those two books.
 
-If you buy 3 different books, you get a 10% discount.
+ ----------------
+ To pass the fourth test, check if given books have the same title. If they don't, apply the discount.
+ You can safely assume that you have at most two books for this test
+ ----------------
 
-If you buy 4 different books, you get a 20% discount.
+ If you buy 3 different books, you get a 10% discount.
 
-If you go the whole hog, and buy all 5, you get a huge 25% discount.
+ If you buy 4 different books, you get a 20% discount.
 
-Note that if you buy, say, four books, of which 3 are different titles,
+ If you go the whole hog, and buy all 5, you get a huge 25% discount.
+
+ Note that if you buy, say, four books, of which 3 are different titles,
  you get a 10% discount on the 3 that form part of a set, but the fourth book still costs 8 EUR.
 
-Your mission is to write a piece of code to calculate the price of any conceivable shopping basket
+ Your mission is to write a piece of code to calculate the price of any conceivable shopping basket
  (containing only Harry Potter books), giving as big a discount as possible.
 
-For example, how much does this basket of books cost?
+ For example, how much does this basket of books cost?
 
-2 copies of the first book
-2 copies of the second book
-2 copies of the third book
-1 copy of the fourth book
-1 copy of the fifth book
+ 2 copies of the first book
+ 2 copies of the second book
+ 2 copies of the third book
+ 1 copy of the fourth book
+ 1 copy of the fifth book
 
-One way of group these 8 books is:
-1 group of 5 --> 25% discount (1st,2nd,3rd,4th,5th)
-+1 group of 3 --> 10% discount (1st,2nd,3rd)
-This would give a total of
-5 books at a 25% discount
-+3 books at a 10% discount
-Giving
-5 x (8 - 2.00) == 5 x 6.00 == 30.00
-+3 x (8 - 0.80) == 3 x 7.20 == 21.60
-For a total of 51.60
+ One way of group these 8 books is:
+ 1 group of 5 --> 25% discount (1st,2nd,3rd,4th,5th)
+ +1 group of 3 --> 10% discount (1st,2nd,3rd)
+ This would give a total of
+ 5 books at a 25% discount
+ +3 books at a 10% discount
+ Giving
+ 5 x (8 - 2.00) == 5 x 6.00 == 30.00
+ +3 x (8 - 0.80) == 3 x 7.20 == 21.60
+ For a total of 51.60
 
-However, a different way to group these 8 books is:
-1 group of 4 books --> 20% discount  (1st,2nd,3rd,4th)
-+1 group of 4 books --> 20% discount  (1st,2nd,3rd,5th)
-This would give a total of
-4 books at a 20% discount
-+4 books at a 20% discount
-Giving
-4 x (8-1.60) == 4 x 6.40 == 25.60
-+4 x (8-1.60) == 4 x 6.40 == 25.60
-For a total of 51.20
+ However, a different way to group these 8 books is:
+ 1 group of 4 books --> 20% discount  (1st,2nd,3rd,4th)
+ +1 group of 4 books --> 20% discount  (1st,2nd,3rd,5th)
+ This would give a total of
+ 4 books at a 20% discount
+ +4 books at a 20% discount
+ Giving
+ 4 x (8-1.60) == 4 x 6.40 == 25.60
+ +4 x (8-1.60) == 4 x 6.40 == 25.60
+ For a total of 51.20
 
-And 51.20 is the price with the biggest discount.
-*/
+ And 51.20 is the price with the biggest discount.
+ */
 
 const BOOKS = ["A", "B", "C", "D", "E"];
 
+function isInvalid(book) {
+    return !BOOKS.includes(book);
+}
+
 module.exports = function resolve(input) {
-    if(!BOOKS.includes(input))
-        return 'ERROR'
-    return 8
+    let cartCost = 0;
+    for (let book of input.split(' ')) {
+        if (isInvalid(book))
+            return 'ERROR'
+        cartCost += 8;
+    }
+    return cartCost;
 }
